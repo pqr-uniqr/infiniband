e7d059b7c017acc3279e178c5d20f25395ff0216
pqhwan <hyun_sik_kim@brown.edu>
2014-10-08 16:59:12 -0400

    segfault hunting1

--- basic_network_measures/rdma_driver.sh
+++ basic_network_measures/rdma_driver.sh
@@ -1,83 +1,13 @@
 #!/bin/bash
 
 ADDRESS=$1
-
-# run RDMA WRITE
-#latency
-./rdma -v w -t 10000 -b 0 $ADDRESS >> write.dat
-sleep 1
-
-./rdma -v w -t 10000 -b 0 $ADDRESS >> write.dat
-sleep 1
-
-./rdma -v w -t 10000 -b 0 $ADDRESS >> write.dat
-sleep 1
-
-./rdma -v w -t 10000 -b 0 $ADDRESS >> write.dat
-sleep 1
-
-./rdma -v w -t 10000 -b 0 $ADDRESS >> write.dat
-sleep 1
-
-#bandwidth
-./rdma -v w -b 10 -t 5000 $ADDRESS >> write.dat
-sleep 1
-./rdma -v w -b 15 -t 1000 $ADDRESS >> write.dat
-sleep 1
-./rdma -v w -b 20 -t 500  $ADDRESS >> write.dat
-sleep 1
-./rdma -v w -b 25 -t 100 $ADDRESS >> write.dat
-sleep 1
-
-# run SEND
-#latency
-./rdma -v r -t 10000 -b 0 $ADDRESS >> send.dat
-sleep 1
-
-./rdma -v r -t 10000 -b 0 $ADDRESS >> send.dat
-sleep 1
-
-./rdma -v r -t 10000 -b 0 $ADDRESS >> send.dat
-sleep 1
-
-./rdma -v r -t 10000 -b 0 $ADDRESS >> send.dat
-sleep 1
-
-./rdma -v r -t 10000 -b 0 $ADDRESS >> send.dat
-sleep 1
-
-#bandwidth
-./rdma -v r -b 10 -t 5000 $ADDRESS >> send.dat
-sleep 1
-./rdma -v r -b 15 -t 1000 $ADDRESS >> send.dat
-sleep 1
-./rdma -v r -b 20 -t 500  $ADDRESS >> send.dat
-sleep 1
-./rdma -v r -b 25 -t 100 $ADDRESS >> send.dat
-sleep 1
-
-exit
+VERB=$2
+CRITERIA=$3
+UPTO=15
 
 
-# run RDMA READ
-#latency
-./rdma -v r -t 10000 -b 0 $ADDRESS  >> read.dat
-sleep 1
-./rdma -v r -t 10000 -b 0 $ADDRESS  >> read.dat
-sleep 1
-./rdma -v r -t 10000 -b 0 $ADDRESS  >> read.dat
-sleep 1
-./rdma -v r -t 10000 -b 0 $ADDRESS  >> read.dat
-sleep 1
-./rdma -v r -t 10000 -b 0 $ADDRESS  >> read.dat
-sleep 1
-#bandwidth
-./rdma -v r -b 10 -t 5000 $ADDRESS >> read.dat
-sleep 1
-./rdma -v r -b 15 -t 1000 $ADDRESS >> read.dat
-sleep 1
-./rdma -v r -b 20 -t 500 $ADDRESS >> read.dat
-sleep 1
-./rdma -v r -b 25 -t 100 $ADDRESS >> read.dat
-sleep 1
-
+for i in `seq 1 $UPTO`;
+do
+  ./rdma -v $VERB -i 10000 -b $i -c $CRITERIA $ADDRESS
+  sleep 1
+done
