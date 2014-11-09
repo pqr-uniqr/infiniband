2bb925653f48b455a22aeae458341e39464e171e
pqhwan <hyun_sik_kim@brown.edu>
2014-11-09 08:35:43 -0500

    wrong format char

--- basic_network_measures/ip.c
+++ basic_network_measures/ip.c
@@ -368,7 +368,7 @@ static void print_report( void )
     double cycles_per_sec = get_cpu_mhz(0) * 1000000;
     double cpu_usage = (ccompleted - cposted) / (cycles_per_sec * (elapsed / 0x1000000) );
 
-    printf("cycles used: %f\n", ccompleted - cposted);
+    printf("cycles used: %lu\n", ccompleted - cposted);
     printf("time elapsed %2.7f\n", elapsed);
     printf("all cycles in time elapsed %f", cycles_per_sec * (elapsed / 0x1000000));
 
