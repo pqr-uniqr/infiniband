cfb1d90bf311817c60fa443a8723c510946f790b
pqhwan <hyun_sik_kim@brown.edu>
2014-12-22 17:06:07 +0900

    typo

--- basic_network_measures/rdma.c
+++ basic_network_measures/rdma.c
@@ -430,9 +430,9 @@ run_iter_server(void *param)
     DEBUG_PRINT((stdout, "[thread %u] posting initial recv WR\n", (int) thread));
 
     //FIXME this might leave us with uncompleted RRs, but for now we're not concerned
-    initial_recv_count = MIN(MAX_SEND_WR, config.iter);
+    initial_recv_count = MIN(MAX_SEND_WR * 2, config.iter);
     DEBUG_PRINT((stdout, "number of initial RRs to be posted: %d\n", initial_recv_count));
-    for(i=0; i < MAX_SEND_WR * 2 ; i++){
+    for(i=0; i < initial_recv_count ; i++){
         if( errno = ibv_post_recv(conn->qp, &rr, &bad_wr) ){
             perror("ibv_post_recv");
             return -1;
