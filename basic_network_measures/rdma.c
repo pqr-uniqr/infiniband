44dd4d9d6c740a204df3170e6ef1e61c501fa502
pqhwan <hyun_sik_kim@brown.edu>
2015-02-03 18:15:18 -0500

    we lost socket 19875 due to power outage

--- basic_network_measures/rdma.c
+++ basic_network_measures/rdma.c
@@ -10,7 +10,7 @@ struct config_t config =
 {
     NULL,               /* dev_name */
     NULL,               /* server_name */
-    19875,          /* tcp_port */
+    19876,          /* tcp_port */
     1,              /* ib_port */
     -1,              /* gid_idx */
     0,              /* xfer_unit */
