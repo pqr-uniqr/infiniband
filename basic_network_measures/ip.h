f385d53ad73981b279264d26a54f7c0cfdf461cb
pqhwan <hyun_sik_kim@brown.edu>
2015-05-08 00:47:26 -0400

    unified report format for ip

--- basic_network_measures/ip.h
+++ basic_network_measures/ip.h
@@ -49,7 +49,7 @@
 #define MAX(X,Y) ((X) < (Y) ? (Y) : (X) )
 #define MIN(X,Y) ((X) < (Y) ? (X) : (Y) )
 
-#define REPORT_FMT "%d\t%d\t%d\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\n"
+#define REPORT_FMT "%d  %d  %d  %.2f  %.2f  %.2f  %.2f  %.2f  %.2f\n"
 
 // part 1: threads, xfer_size, iter (setup)
 #define MTHREAD_RPT_PT1 "%d  %d  %ld"
